1a4057d886594dc62963cea66aa9bfdedfae92f0
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2021-08-10 02:17:35 +0000

    Listo ejercicio Factorial

--- assignments/00Factorial/src/exercise.py
+++ assignments/00Factorial/src/exercise.py
@@ -1,17 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    for i in range(3):
-        n=int(input("Dame un numero="))
-
-        if n < 0:
-            print(str(n)+"!=Error!")
-        else:
-            p=1
-            f=1
-            while p <= n:
-                f = f * p
-                p=p+1
-            print(str(n)+"!="+str(f))
+    pass
 
 if __name__=='__main__':
     main()
